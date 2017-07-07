745dace485a724dcda6f88db32956f1a45ca51c7
Hannes Calitz <hannesc@grandmark.com>
2017-07-07 15:55:25 +0200

    ...

---
+++ PWATest/Scripts/test.js
@@ -0,0 +1,70 @@
+﻿var test = (function (window, $) {
+
+    var me = {};
+    var db;
+
+    me.init = function () {
+
+        $("#go").unbind().bind("click",
+            function () {
+                saveText();
+            });
+
+        //HANNES: Create/Open database
+        var request = window.indexedDB.open("MyTestDatabase", 1);
+
+        request.onerror = function (event) {
+            alert("Database error: " + event.target.errorCode);
+        };
+        request.onsuccess = function (event) {
+            db = event.target.result;
+
+            //Load the data if available
+            var customers = [];
+
+            var objectStore = db.transaction("customers").objectStore("customers");
+
+            objectStore.getAll().onsuccess = function (event) {
+                customers = event.target.result;
+                //alert(customers.length);
+
+                showCustomers(customers);
+            };
+        };
+        request.onupgradeneeded = function () {
+            db = event.target.result;
+            var objectStore = db.createObjectStore("customers", { autoIncrement: true });
+        }
+    }
+
+    return me;
+
+    function showCustomers(customers) {
+        $("#customers").append(JSON.stringify(customers));
+    }
+
+    function saveText() {
+        var transaction = db.transaction(["customers"], "readwrite");
+
+        transaction.oncomplete = function (event) {
+            alert("All done!");
+        };
+
+        transaction.onerror = function (event) {
+            alert("ag poefies");
+        };
+
+        var objectStore = transaction.objectStore("customers");
+
+        var customer = {
+            name: $("#text1").val(),
+            age: $("#text2").val()
+        }
+
+        var request = objectStore.add(customer);
+        request.onsuccess = function(event) {
+            alert(event.key);
+        }
+    }
+
+})(window, jQuery);
--- PWATest/sw.js
+++ PWATest/sw.js
@@ -1,34 +1,5 @@
-﻿//(global => {
-//    "use strict";
-//    importScripts("/node_modules/sw-toolbox/sw-toolbox.js");
-
-//    // Ensure that our service worker takes control of the page as soon as possible.
-//    //global.addEventListener("install", event => event.waitUntil(global.skipWaiting()));
-//    //global.addEventListener("activate", event => event.waitUntil(true));
-
-//    global.toolbox.options.debug = true;
-//    toolbox.precache([
-//        "/index.html"
-//    ] );
-//    toolbox.router.get("/index.html", toolbox.cacheFirst, {
-//        cache: {
-//            name: "siteCache",
-//            maxEntries: 1000000,
-//            maxAgeSeconds: 172800
-//        }
-//    });
-
-//    //toolbox.router.get("/images/*", global.toolbox.cacheFirst);
-//    //toolbox.router.get("/*", global.toolbox.networkFirst, { networkTimeoutSeconds: 5 });
-//    //toolbox.router.get("/", global.toolbox.offline);
-//})(self);
-
-"use strict";
+﻿"use strict";
 importScripts("node_modules/sw-toolbox/sw-toolbox.js");
-
-// Ensure that our service worker takes control of the page as soon as possible.
-//global.addEventListener("install", event => event.waitUntil(global.skipWaiting()));
-//global.addEventListener("activate", event => event.waitUntil(true));
 
 toolbox.options.debug = true;
 toolbox.precache([
